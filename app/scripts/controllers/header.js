59266430980f7ed4c3538fb32015db20b67cfd38
Lance Cookson <lcookson@rmn.com>
2014-10-09 09:14:58 -0500

    Got sub-menu working

---
+++ app/scripts/controllers/header.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name mobileProtoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the mobileProtoApp
+ */
+angular.module('mobileProtoApp')
+  .controller('HeaderCtrl', function ($scope) {
+
+    $scope.toggleActive = function(){
+    	$scope.check = $scope.check === false ? true: false;
+    }
+  });
