b37a3778448c6c04646ce5f93d0d869c8c9b5c1c
Lance Cookson <lcookson@rmn.com>
2014-10-21 14:54:31 -0500

    Beginning infinite scroll

--- app/scripts/controllers/infinite.js
+++ app/scripts/controllers/infinite.js
@@ -10,5 +10,11 @@
 angular.module('mobileProtoApp')
   .controller('InfiniteCtrl', ['$scope','Infinite',
    function ($scope, Infinite) {
+   	var infiniteLimit = 10;
     $scope.infinite = Infinite.query();
+    $scope.infiniteLimit = infiniteLimit;
+    $scope.incrementLimit = function(){
+    	var last = infiniteLimit;
+ 			$scope.infiniteLimit.push(last + 4);
+    };
   }]);
